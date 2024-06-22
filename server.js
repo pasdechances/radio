a3dc65769f32155a68b0d1cbb329cdb1e8d58a21
pasdechances <araujo.ludovic@gmail.com>
2024-06-22 16:22:36 +0200

    adding room concept

--- server.js
+++ server.js
@@ -2,33 +2,59 @@ const express = require('express');
 const path = require('path');
 const socketIo = require('socket.io');
 const http = require('http');
-const { streamFile } = require('./function');
+const { leaveAllRooms, switchRoom, roomClients } = require('./socketFunctions');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const port = 3000;
 
+
 io.on('connection', (socket) => {
   console.log('New client connected to lobby');
 
   socket.on('userMessage', (msg) => {
-    console.log(msg);
-    io.emit('serverMessage', msg);
+    const rooms = Object.keys(socket.rooms).filter(r => r !== socket.id);
+    if (rooms.length > 0) {
+      rooms.forEach(room => {
+        io.to(room).emit('serverMessage', msg);
+      });
+    } else {
+      socket.emit('serverMessage', msg); // If no room, send only to the sender
+    }
+  });
+
+  socket.on('GotoRoom1', () => {
+    switchRoom(socket, "Room1", io);
+  });
+
+  socket.on('GotoRoom2', () => {
+    switchRoom(socket, "Room2", io);
+  });
+
+  socket.on('GotoRoom3', () => {
+    switchRoom(socket, "Room3", io);
+  });
+
+  socket.on('GotoLobby', () => {
+    leaveAllRooms(socket, io);
   });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
+    leaveAllRooms(socket, io);
   });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/lecteur', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 server.listen(port, () => {
   console.log(`Web radio server running at http://localhost:${port}`);
-  streamFile(io);
 });
+
+
+
---
+++ socketFunctions.js
@@ -0,0 +1,37 @@
+const { streamFile } = require('./streamFunctions');
+const roomClients = {};
+
+const switchRoom = (socket, room, io) => {
+  leaveAllRooms(socket);
+
+  socket.join(room);
+  if (!roomClients[room]) {
+    roomClients[room] = 0;
+  }
+  roomClients[room]++;
+
+  if (roomClients[room] === 1) {
+    streamFile(io.to(room)); // Stream only to the room
+  }
+
+  console.log(`Client joined ${room}`);
+};
+
+const leaveAllRooms = (socket, io) => {
+  const rooms = Object.keys(socket.rooms).filter(r => r !== socket.id);
+
+  rooms.forEach(room => {
+    socket.leave(room);
+    roomClients[room]--;
+
+    if (roomClients[room] <= 0) {
+      delete roomClients[room];
+      // Stop streaming logic here if needed
+      console.log(`No more clients in ${room}. Streaming stopped.`);
+    }
+  });
+
+  console.log(`Client left all rooms: ${rooms}`);
+};
+
+module.exports = { leaveAllRooms, switchRoom, roomClients };
